274af48914a73f1bc1d1403b0eea6a44ad76a6e2
Darma <darma8824@gmail.com>
2022-09-13 14:06:26 +0700

    update ssr

--- gatsby-config.js
+++ gatsby-config.js
@@ -57,6 +57,7 @@ module.exports = {
       },
     },
     "gatsby-plugin-sitemap",
+    "gatsby-plugin-styled-components",
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // 'gatsby-plugin-offline',
